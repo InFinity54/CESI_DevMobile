e549292164872f399522154ac60c48fe8bee689f
InFinity_54 <corentinkreicher@gmail.com>
2020-08-21 08:48:01 +0200

    Classe Champion + image de tous les champions + tri par ordre alphabétique des champions

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,64 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final champions = [
-    [
-      "Ahri",
-      "assets/images/champions/Ahri.jpg"
-    ],
-    [
-      "Vel'Koz le BG",
-      "assets/images/champions/Vel'Koz.jpg"
-    ],
-    [
-      "Aatrox",
-      "assets/images/champions/Aatrox.jpg"
-    ],
-    [
-      "Malphite",
-      "assets/images/champions/Malphite.jpg"
-    ],
-    [
-      "Sona",
-      "assets/images/champions/Sona.jpg"
-    ]
+    new Champion(
+        "Ahri",
+        "Ahri",
+        "", // description
+        "", // content
+        0, // category
+        "assets/images/champions/Ahri.jpg",
+        7, // rating
+        true
+    ),
+    new Champion(
+        "Velkoz",
+        "Vel'Koz",
+        "", // description
+        "", // content
+        0, // category
+        "assets/images/champions/Velkoz.jpg",
+        7, // rating
+        true
+    ),
+    new Champion(
+        "Aatrox",
+        "Aatrox",
+        "", // description
+        "", // content
+        0, // category
+        "assets/images/champions/Aatrox.jpg",
+        7, // rating
+        true
+    ),
+    new Champion(
+        "Malphite",
+        "Malphite",
+        "", // description
+        "", // content
+        0, // category
+        "assets/images/champions/Malphite.jpg",
+        7, // rating
+        true
+    ),
+    new Champion(
+        "Sona",
+        "Sona",
+        "", // description
+        "", // content
+        0, // category
+        "assets/images/champions/Sona.jpg",
+        7, // rating
+        true
+    )
   ];
 
   @override
   Widget build(BuildContext context) {
     final title = 'Liste des personnages';
+    champions.sort((a, b) {
+      return a.id.compareTo(b.id);
+    });
 
     return MaterialApp(
       title: title,
@@ -44,15 +77,15 @@ class MyApp extends StatelessWidget {
           children: List.generate(champions.length, (index){
             return new GestureDetector(
                 onTap: (){
-                  print("Clic sur " + champions[index][0].toString() + " !");
+                  print("Clic sur " + champions[index].getTitle + " !");
                 },
                 child: new Container(
                     padding: new EdgeInsets.all(35.0),
                     child: new Column(
                         children: [
-                          Expanded(child: new Image.asset(champions[index][1].toString())),
+                          Expanded(child: new Image.asset(champions[index].getImage)),
                           Text(
-                            champions[index][0].toString(),
+                            champions[index].getTitle,
                             style: Theme.of(context).textTheme.headline5,
                           )
                         ]
@@ -63,5 +96,36 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class Champion {
+  String id;
+  String title;
+  String description;
+  String content;
+  int category;
+  String image;
+  int rating;
+  bool status;
+
+  Champion(String id, String title, String description, String content, int category, String image, int rating, bool status)
+  {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.content = content;
+    this.category = category;
+    this.image = image;
+    this.rating = rating;
+    this.status = status;
+  }
+
+  String get getTitle {
+    return this.title;
+  }
+
+  String get getImage {
+    return this.image;
   }
 }
